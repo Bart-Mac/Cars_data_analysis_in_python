d114a3491c89c76df04dad046b726be54ca1ede1
Bart-Mac <164554881+Bart-Mac@users.noreply.github.com>
2024-04-12 14:47:42 +0200

    Update Importing data.py

--- Importing data.py
+++ Importing data.py
@@ -3,6 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import scipy.stats as st
-cars_data = pd.read_csv('C:/Users/barte/Desktop/OLX_cars_dataset00.csv') #importing data
-print(cars_data.info()) # checking a type of data imported
-print(cars_data.corr(numeric_only=True)) # checking basic correlation between numeric type of data
+cars_data = pd.read_csv('C:/Users/barte/Desktop/Sport car price.csv') #importing data
+print(cars_data.info()) # checking data type

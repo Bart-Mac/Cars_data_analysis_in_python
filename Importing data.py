1351566e6c05a1e91dcefda5e299406234e0f3b9
Bart-Mac <164554881+Bart-Mac@users.noreply.github.com>
2024-04-13 14:48:25 +0200

    Update Importing data.py

--- Importing data.py
+++ Importing data.py
@@ -3,5 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import scipy.stats as st
-cars_data = pd.read_csv('C:/Users/barte/Desktop/Sport car price.csv') #importing data
+cars_data = pd.read_csv('/Sport car price.csv') #importing data
 print(cars_data.info()) # checking data type

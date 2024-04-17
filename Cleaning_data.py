2a516433839c9edb8adc407e05e22ad4893271f8
Bart-Mac <164554881+Bart-Mac@users.noreply.github.com>
2024-04-17 09:06:58 +0200

    Update Cleaning_data.py

--- Cleaning_data.py
+++ Cleaning_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import scipy.stats as st
+import seaborn as sns
 cars_data['Horsepower'] = cars_data['Horsepower'].str.replace(',', '.').str.replace('+','')
 cars_data['Horsepower'] = pd.to_numeric(cars_data['Horsepower'])
 print(cars_data.info())

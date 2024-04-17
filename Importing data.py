74a4efb2f601d5e27714120bc64dcd2dbffe93b2
Bart-Mac <164554881+Bart-Mac@users.noreply.github.com>
2024-04-17 09:06:49 +0200

    Update Importing data.py

--- Importing data.py
+++ Importing data.py
@@ -3,5 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import scipy.stats as st
+import seaborn as sns
 cars_data = pd.read_csv('/Sport car price.csv') #importing data
 print(cars_data.info()) # checking data type

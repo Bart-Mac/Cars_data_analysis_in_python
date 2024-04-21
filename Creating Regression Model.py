f4d06a82dd489add6578721767290b999bf01327
Bart-Mac <164554881+Bart-Mac@users.noreply.github.com>
2024-04-21 19:35:46 +0200

    Update Creating Regression Model.py

--- Creating Regression Model.py
+++ Creating Regression Model.py
@@ -1,40 +1,74 @@
-Cor = cars_data.corr(numeric_only=True)
-sns.heatmap(Cor, cmap="YlGnBu", annot=True, cbar=True)
+CORREL = cars_data.corr(numeric_only=True)
+sns.heatmap(CORREL, cmap="YlGnBu", annot=True, cbar=True)
 plt.show()
-cars_data.dropna(inplace=True)
-x=  cars_data[['Year', 'Engine Size (L)', 'Horsepower',  '0-60 MPH Time (seconds)']]
-y= cars_data['Price (in USD)']
-x =sm.add_constant(x)
-model =sm.OLS(y, x.astype(float)).fit()
+pd.to_numeric(cars_data['Engine Size (L)'], errors='coerce')
+cars_data.dropna(how='any', inplace=True)
+X = cars_data[["Year", "Horsepower", "Torque (lb-ft)", "0-60 MPH Time (seconds)", "Electric", "Hybrid"]]
+Y = cars_data["Price (in USD)"]
+X =sm.add_constant(X)
+model = sm.OLS(Y, X.astype(float)).fit()
 print(model.summary())
 
 '''
- OLS Regression Results                            
+OLS Regression Results                            
 ==============================================================================
-Dep. Variable:         Price (in USD)   R-squared:                       0.609
-Model:                            OLS   Adj. R-squared:                  0.607
-Method:                 Least Squares   F-statistic:                     293.0
-Date:                Wed, 17 Apr 2024   Prob (F-statistic):          4.87e-189
-Time:                        08:53:12   Log-Likelihood:                -13647.
-No. Observations:                 948   AIC:                         2.731e+04
-Df Residuals:                     942   BIC:                         2.733e+04
-Df Model:                           5                                         
+Dep. Variable:         Price (in USD)   R-squared:                       0.626
+Model:                            OLS   Adj. R-squared:                  0.623
+Method:                 Least Squares   F-statistic:                     274.8
+Date:                Sun, 21 Apr 2024   Prob (F-statistic):          1.39e-206
+Time:                        18:24:25   Log-Likelihood:                -14343.
+No. Observations:                 994   AIC:                         2.870e+04
+Df Residuals:                     987   BIC:                         2.873e+04
+Df Model:                           6                                         
 Covariance Type:            nonrobust                                         
 ===========================================================================================
                               coef    std err          t      P>|t|      [0.025      0.975]
 -------------------------------------------------------------------------------------------
-const                    4.183e+07    1.4e+07      2.978      0.003    1.43e+07    6.94e+07
-Year                    -2.155e+04   6947.882     -3.102      0.002   -3.52e+04   -7917.520
-Engine Size (L)         -5.017e+04    1.3e+04     -3.874      0.000   -7.56e+04   -2.48e+04
-Horsepower               2640.5774    173.760     15.197      0.000    2299.576    2981.579
-Torque (lb-ft)            294.6420    206.491      1.427      0.154    -110.593     699.877
-0-60 MPH Time (seconds)  1.635e+05   2.83e+04      5.786      0.000    1.08e+05    2.19e+05
+const                    2.943e+07   1.42e+07      2.076      0.038    1.61e+06    5.72e+07
+Year                    -1.521e+04   7013.338     -2.169      0.030    -2.9e+04   -1452.155
+Horsepower               2115.5007    139.888     15.123      0.000    1840.989    2390.012
+Torque (lb-ft)            214.9037    169.197      1.270      0.204    -117.123     546.930
+0-60 MPH Time (seconds)   8.29e+04   2.62e+04      3.162      0.002    3.14e+04    1.34e+05
+Electric                -6.356e+05   9.02e+04     -7.044      0.000   -8.13e+05   -4.59e+05
+Hybrid                  -1.199e+04   3.18e+05     -0.038      0.970   -6.35e+05    6.11e+05
 ==============================================================================
-Omnibus:                      836.716   Durbin-Watson:                   1.739
-Prob(Omnibus):                  0.000   Jarque-Bera (JB):            32459.291
-Skew:                           3.887   Prob(JB):                         0.00
-Kurtosis:                      30.592   Cond. No.                     2.16e+06
+Omnibus:                      761.779   Durbin-Watson:                   1.828
+Prob(Omnibus):                  0.000   Jarque-Bera (JB):            16661.240
+Skew:                           3.341   Prob(JB):                         0.00
+Kurtosis:                      21.911   Cond. No.                     2.18e+06
 ==============================================================================
-1. The Torque (lb-ft) variable turned out to be statistically insignificant, so I then removed it from the model
-2. 60% of the variability of the explanatory variable was explained by the model
+1. The Torque (lb-ft) and hybrid variables turned out to be statistically insignificant, so I then removed it from the model
+2. 62% of the variability of the explanatory variable was explained by the model
+
+
+Corrected model
+OLS Regression Results                            
+==============================================================================
+Dep. Variable:         Price (in USD)   R-squared:                       0.625
+Model:                            OLS   Adj. R-squared:                  0.623
+Method:                 Least Squares   F-statistic:                     412.0
+Date:                Sun, 21 Apr 2024   Prob (F-statistic):          7.54e-209
+Time:                        18:27:20   Log-Likelihood:                -14343.
+No. Observations:                 994   AIC:                         2.870e+04
+Df Residuals:                     989   BIC:                         2.872e+04
+Df Model:                           4                                         
+Covariance Type:            nonrobust                                         
+===========================================================================================
+                              coef    std err          t      P>|t|      [0.025      0.975]
+-------------------------------------------------------------------------------------------
+const                    2.928e+07   1.42e+07      2.066      0.039    1.47e+06    5.71e+07
+Year                    -1.513e+04   7011.444     -2.158      0.031   -2.89e+04   -1368.505
+Horsepower               2267.5737     72.420     31.312      0.000    2125.460    2409.687
+0-60 MPH Time (seconds)  8.242e+04   2.62e+04      3.145      0.002     3.1e+04    1.34e+05
+Electric                -6.087e+05   8.77e+04     -6.941      0.000   -7.81e+05   -4.37e+05
+==============================================================================
+Omnibus:                      767.583   Durbin-Watson:                   1.826
+Prob(Omnibus):                  0.000   Jarque-Bera (JB):            16795.671
+Skew:                           3.381   Prob(JB):                         0.00
+Kurtosis:                      21.969   Cond. No.                     2.11e+06
+==============================================================================
+1. All variables are statistically significant
+2. 62% of the variability of the explanatory variable was explained by the model
 '''
+
+
